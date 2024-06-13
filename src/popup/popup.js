5a52ffda376febdc7fe27f6e4d9deda42fc12013
karol-121 <karol.turlej@gmail.com>
2024-06-13 20:34:10 +0200

    general structure improvements

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,34 +1,51 @@
 const qr_img = document.getElementById('qr');
 qr_img.addEventListener('click', resize);
 
-//get url of current tab
+//qr placeholder for error
+const bad_url = browser.runtime.getURL("assets/qr-error.png");
+
+//get url of current tab by sending an request to background script
 browser.runtime.sendMessage({request: "getCurrentURL"}).then(resolve, onError);
 
+//function that handles positive response for request
 function resolve(response) {
-	printQR(response);
+	const qr_data = generateQRData(response); 
+	printQR(qr_data);
 }
 
+//function that handles negative response for request
 function onError(error) {
+	const qr_data = {url: bad_url, alt: "Something went wrong"};
+	printQR(qr_data);
 	console.log(error);
 }
 
-function printQR(url) {
-	const api_url = new URL("https://api.qrserver.com/v1/create-qr-code/");
+
+//generates qr code url and alt text for provided url
+function generateQRData(url) {
+	//todo: add check here, ie if link is empty or if it is to long etc.
 
 	//todo: set size based on the lenght of the link, the longer the link is, the bigger the qr code has to be, 
 	// therefore it needs to be bigger
+	const api_url = new URL("https://api.qrserver.com/v1/create-qr-code/");
 	api_url.searchParams.append("size", "150x150");
 	api_url.searchParams.append("format", "svg");
 	api_url.searchParams.append("data", url);
 
-	qr_img.src = api_url;
+	return { url: api_url, alt: "QR code for current tab"};
+}
+
+//prints qr data to dom
+function printQR(qr_data) {
+	qr_img.src = qr_data.url;
+	qr_img.alt = qr_data.alt;
 }
 
 function resize() {
 	if(qr_img.className === "zoom-2x") {
 		qr_img.className = "zoom-normal";
 	} else {
-		qr_img.className = "zoom-2x";	
+		qr_img.className = "zoom-2x";
 	}
 }
 
