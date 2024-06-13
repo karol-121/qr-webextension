3e48ee29bee746a5483d0199b62e5f325e1d1558
karol-121 <karol.turlej@gmail.com>
2024-06-13 14:51:05 +0200

    mainly working code

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,14 +1,37 @@
-//here make message an object with parameters
+const qr_img = document.getElementById('qr');
+qr_img.addEventListener('click', resize);
+
+//get url of current tab
 browser.runtime.sendMessage({request: "getCurrentURL"}).then(resolve, onError);
 
-
-function resolve(msg) {
-	console.log(msg);
+function resolve(response) {
+	printQR(response);
 }
 
-function onError(err) {
-	console.log(err);
+function onError(error) {
+	console.log(error);
 }
+
+function printQR(url) {
+	const api_url = new URL("https://api.qrserver.com/v1/create-qr-code/");
+
+	//todo: set size based on the lenght of the link, the longer the link is, the bigger the qr code has to be, 
+	// therefore it needs to be bigger
+	api_url.searchParams.append("size", "150x150");
+	api_url.searchParams.append("format", "svg");
+	api_url.searchParams.append("data", url);
+
+	qr_img.src = api_url;
+}
+
+function resize() {
+	if(qr_img.className === "zoom-2x") {
+		qr_img.className = "zoom-normal";
+	} else {
+		qr_img.className = "zoom-2x";	
+	}
+}
+
 
 
 
