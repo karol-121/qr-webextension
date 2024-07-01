0c68dbb8f5b9f153d217beccb7fba4b9b56411b6
karol-121 <karol.turlej@gmail.com>
2024-07-01 14:45:38 +0200

    adaptive code size
    + some improvements in structure

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -9,35 +9,32 @@ const qr_object = {
 		this.dom_element.alt = this.alt;
 		this.dom_element.width = this.size;
 		this.dom_element.height = this.size;
-	},
-	resize: function () {
-		//todo: make qr code resizable
 	}
 }
-
-//attach resize function to onclick event
-qr_object.dom_element.addEventListener('click', function() {
-	qr_object.resize();
-});
-
 
 //get url of current tab by sending an request to background script
 browser.runtime.sendMessage({request: "getCurrentURL"}).then(resolve, onError);
 
 //function that handles positive response for request
 function resolve(response) {
+	//verify that link exist or is not empty
 	if (!response) {
-		qr_object.alt = "missing link";
+		onError("Response is either undefined or is empty string", "Something went wrong :(");
 		return;
 	}
 
-	if (response.length > 256) {
-		qr_object.alt = "link too long";
-		return;
-	}
-
-	//define qr code size based on lenght of the url
+	//match size of the qr code according to link length
 	const defined_size = defineSize(response);
+
+	//check if link length does not exceed maximum
+	if (defined_size === -1) {
+		onError("Link is too long", "Link is too long :(");
+		return;
+	}
+
+	//at this point the link should be valid and size of the qr code set
+	//proceed to create qr code and print it
+
 	const api_url = createURL(response, defined_size);
 
 	qr_object.url = api_url;
@@ -48,17 +45,39 @@ function resolve(response) {
 }
 
 //function that handles negative response for request
-function onError(error) {
-	qr_object.alt = "Something went wrong";
+function onError(error, message) {
+	qr_object.alt = (!message) ? "Something went wrong" : message;
 	qr_object.print();
 	console.log(error);
 }
 
-
-//todo: set size based on the lenght of the link, the longer the link is, the bigger the qr code has to be, 
-// therefore it needs to be bigger
 function defineSize(url) {
-	return 300;
+	//good practice: keep size of the qr code up to level 4
+	//any bigger qr code may be difficult for the phones to scan
+
+	/*qr code levels:
+		1 = 21 modules, 25 alphanumeric
+		2 - 25 modules, 47 alphanumeric
+		3 - 29 modules, 77 alphanumeric
+		4 - 33 modules, 114 alphanumeric
+		*given ecc level low
+	*/
+
+	//size chart for qr code based on amount of data
+	//the size of qr code level 2 and 4 are adjusted to look better
+	const sizes = [[25, 21],[47, 29],[77, 29],[114, 37]];
+
+	for (size of sizes) {
+		if (url.length <= size[0]) {
+			return size[1] * 10;
+		}
+	}
+
+	//here we have to address links that are longer than 114 chars
+	//we could try to trim it and then try to define size again
+	//if this fail then we can print error about link being too long
+
+	return -1;
 }
 
 //function that creates url for qr code
